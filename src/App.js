ca01d537a200dac66f7097ba91ee61c7fa064a3f
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-03-09 16:48:08 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  MapContainer,
+  TileLayer,
+  LayerGroup,
+  Marker,
+  Popup,
+} from "react-leaflet";
+import useStyles from "./useStyles";
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MapContainer
+        className={classes.mapContainer}
+        center={[-28.995409163308832, 25.093833387362697]}
+        zoom={6}
+        scrollWheelZoom={false}
+        preferCanvas={true}
+        zoomControl={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png"
+          zIndex={200}
+          pane="tilePane"
+        />
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}.png"
+          zIndex={350}
+          pane="tilePane"
+        />
+        <LayerGroup>
+          {/* <TileLayer
+            // url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}.png"
+            bounds={{
+              a: { lat: -30.0018, lng: 30.1939 },
+              b: { lat: -34.2128, lng: 22.7358 },
+            }}
+            pane="overlayPane"
+          /> */}
+        </LayerGroup>
+
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
---
+++ src/useStyles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  mapContainer: {
+    width: "100%",
+    height: "900px",
+  },
+});
+
+export default useStyles;
